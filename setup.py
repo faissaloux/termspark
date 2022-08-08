95c714ed71d49a92e6f90f43fd4406e7524440cd
faissaloux <fwahabali@gmail.com>
2022-08-08 18:29:01 +0100

    update vesion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='termspark',
-    version='0.0.5',
+    version='0.0.6',
     author='Faissal Wahabali',
     author_email='fwahabali@gmail.com',
     description='Takes control of terminal',
