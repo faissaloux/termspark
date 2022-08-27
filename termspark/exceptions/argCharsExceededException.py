b3e7d68535e4e7eed29e2f77b6c184b46ece7e19
faissaloux <fwahabali@gmail.com>
2022-08-27 03:00:19 +0100

    fix duplicate exceptions message

--- termspark/exceptions/argCharsExceededException.py
+++ termspark/exceptions/argCharsExceededException.py
@@ -6,8 +6,5 @@ class ArgCharsExceededException(Exception):
         self.max = max
 
     def __str__(self):
-        termspark.TermSpark().line().spark()
         message = termspark.TermSpark().print_left(f"Sorry, {self.arg} can contain only {self.max} character", 'red')
-        message.spark()
-        termspark.TermSpark().line().spark()
         return str(message)
--- termspark/exceptions/printerArgException.py
+++ termspark/exceptions/printerArgException.py
@@ -5,8 +5,5 @@ class PrinterArgException(Exception):
         self.position = position
 
     def __str__(self):
-        termspark.TermSpark().line().spark()
         message = termspark.TermSpark().spark_left([f" print_{self.position}() ", 'white', 'red'], [f" doesn't accept lists, maybe you wanna use ", 'red'], [f" spark_{self.position}() ", 'white', 'blue'])
-        message.spark()
-        termspark.TermSpark().line().spark()
         return str(message)
