c1204bd651cc296c6d7f889fdc9a3bfba36c05f7
faissaloux <fwahabali@gmail.com>
2022-09-03 16:49:21 +0100

    fix typing errors on python3.7 and python3.8

--- termspark/helpers/list.py
+++ termspark/helpers/list.py
@@ -1,5 +1,7 @@
+from typing import List as TypingList
+
 class List:
-    def snake(self, list: list[str]) -> list[str]:
+    def snake(self, list: TypingList[str]) -> TypingList[str]:
         for index, elem in enumerate(list):
             list[index] = elem.replace(' ', '_') if elem else elem
 
