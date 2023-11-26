6d3ab70fa9a165d32edb155344c25455c7d6c854
faissaloux <fwahabali@gmail.com>
2023-11-26 14:36:33 +0100

    improve tests cov

--- tests/trim_test.py
+++ tests/trim_test.py
@@ -7,7 +7,19 @@ class TestTrim:
         terminal_width = termspark.get_terminal_width()
 
         text = "A" * (terminal_width + 10)
-        termspark = termspark.print_left(text, "red")
+        termspark = termspark.print_left("A" * 8).print_right(text, "red")
+        termspark.spark()
+
+        assert len(text) > len(termspark.left["content"][0])
+
+    def test_trim_when_text_exceeds_terminal_width_on_list(self):
+        termspark = TermSpark()
+        terminal_width = termspark.get_terminal_width()
+
+        text = "A" * (terminal_width + 10)
+        termspark = termspark.spark_left(["A" * 8]).spark_right(
+            [text, "red"], [text, "blue"]
+        )
         termspark.spark()
 
         assert len(text) > len(termspark.left["content"][0])
