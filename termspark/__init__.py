4d9fcf90f46eef8d13da88e2570b079d4dfa2572
faissaloux <fwahabali@gmail.com>
2023-12-07 21:32:05 +0100

    print function

--- termspark/__init__.py
+++ termspark/__init__.py
@@ -1,7 +1,23 @@
 import os
 import sys
+from typing import Optional
 
 from termspark.termspark import TermSpark
 
 if sys.platform == "win32":  # codecov-ignore
     os.system("color")
+
+
+def print(
+    content: Optional[str] = None,
+    color: Optional[str] = None,
+    highlight: Optional[str] = None,
+    style: Optional[str] = None,
+    end: str = "\n",
+) -> None:
+    if content is None:
+        content = ""
+
+    termspark = TermSpark()
+    termspark.print_left(content, color, highlight, style)
+    termspark.spark(end)
