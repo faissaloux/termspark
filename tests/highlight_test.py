6e20091f60b07ba73e886d00c737da47cfebebd0
faissaloux <fwahabali@gmail.com>
2024-06-07 16:19:39 +0100

    support second 50 highlights from 256 colors

--- tests/highlight_test.py
+++ tests/highlight_test.py
@@ -131,8 +131,8 @@ class TestHighlight:
     def test_caribbean_green(self):
         assert Highlight.CARIBBEAN_GREEN == 42
 
-    def test_cyan(self):
-        assert Highlight.CYAN == 43
+    def test_cyan_1(self):
+        assert Highlight.CYAN_1 == 43
 
     def test_dark_turquoise(self):
         assert Highlight.DARK_TURQUOISE == 44
@@ -154,3 +154,153 @@ class TestHighlight:
 
     def test_sea_green(self):
         assert Highlight.SEA_GREEN == 50
+
+    def test_cyan(self):
+        assert Highlight.CYAN
+
+    def test_rosewood(self):
+        assert Highlight.ROSEWOOD
+
+    def test_pompadour(self):
+        assert Highlight.POMPADOUR
+
+    def test_pigment_indigo(self):
+        assert Highlight.PIGMENT_INDIGO
+
+    def test_purple_3(self):
+        assert Highlight.PURPLE_3
+
+    def test_electic_violet(self):
+        assert Highlight.ELECTIC_VIOLET
+
+    def test_blue_violet(self):
+        assert Highlight.BLUE_VIOLET
+
+    def test_verdun_green(self):
+        assert Highlight.VERDUN_GREEN
+
+    def test_scorpion(self):
+        assert Highlight.SCORPION
+
+    def test_comet(self):
+        assert Highlight.COMET
+
+    def test_scampi(self):
+        assert Highlight.SCAMPI
+
+    def test_indigo(self):
+        assert Highlight.INDIGO
+
+    def test_cornflower_blue_1(self):
+        assert Highlight.CORNFLOWER_BLUE_1
+
+    def test_limeade(self):
+        assert Highlight.LIMEADE
+
+    def test_glade_green(self):
+        assert Highlight.GLADE_GREEN
+
+    def test_juniper(self):
+        assert Highlight.JUNIPER
+
+    def test_hippie_blue(self):
+        assert Highlight.HIPPIE_BLUE
+
+    def test_havelock_blue(self):
+        assert Highlight.HAVELOCK_BLUE
+
+    def test_cornflower_blue(self):
+        assert Highlight.CORNFLOWER_BLUE
+
+    def test_limea(self):
+        assert Highlight.LIMEA
+
+    def test_fern(self):
+        assert Highlight.FERN
+
+    def test_silver_tree(self):
+        assert Highlight.SILVER_TREE
+
+    def test_tradewind(self):
+        assert Highlight.TRADEWIND
+
+    def test_shakespeare(self):
+        assert Highlight.SHAKESPEARE
+
+    def test_malibu(self):
+        assert Highlight.MALIBU
+
+    def test_bright_green(self):
+        assert Highlight.BRIGHT_GREEN
+
+    def test_pale_green(self):
+        assert Highlight.PALE_GREEN
+
+    def test_pastel_green(self):
+        assert Highlight.PASTEL_GREEN
+
+    def test_downy(self):
+        assert Highlight.DOWNY
+
+    def test_viking(self):
+        assert Highlight.VIKING
+
+    def test_steel_blue(self):
+        assert Highlight.STEEL_BLUE
+
+    def test_chartreuse(self):
+        assert Highlight.CHARTREUSE
+
+    def test_screaming_green(self):
+        assert Highlight.SCREAMING_GREEN
+
+    def test_sea_green_1(self):
+        assert Highlight.SEA_GREEN_1
+
+    def test_aquamarine_1(self):
+        assert Highlight.AQUAMARINE_1
+
+    def test_aquamarine_2(self):
+        assert Highlight.AQUAMARINE_2
+
+    def test_aquamarine(self):
+        assert Highlight.AQUAMARINE
+
+    def test_dark_red(self):
+        assert Highlight.DARK_RED
+
+    def test_fresh_eggplant(self):
+        assert Highlight.FRESH_EGGPLANT
+
+    def test_dark_magenta(self):
+        assert Highlight.DARK_MAGENTA
+
+    def test_purple_2(self):
+        assert Highlight.PURPLE_2
+
+    def test_electric_violet(self):
+        assert Highlight.ELECTRIC_VIOLET
+
+    def test_purple_1(self):
+        assert Highlight.PURPLE_1
+
+    def test_brown(self):
+        assert Highlight.BROWN
+
+    def test_copper_rose(self):
+        assert Highlight.COPPER_ROSE
+
+    def test_strike_master(self):
+        assert Highlight.STRIKE_MASTER
+
+    def test_deluge(self):
+        assert Highlight.DELUGE
+
+    def test_medium_purple(self):
+        assert Highlight.MEDIUM_PURPLE
+
+    def test_heliotrope(self):
+        assert Highlight.HELIOTROPE
+
+    def test_olive_1(self):
+        assert Highlight.OLIVE_1
