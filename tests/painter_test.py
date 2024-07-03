9cf0da77049f4632cdc5d8785355cd47ff7315c2
faissaloux <fwahabali@gmail.com>
2024-07-03 17:01:46 +0100

    hardcode color codes in tests

--- tests/painter_test.py
+++ tests/painter_test.py
@@ -16,13 +16,13 @@ class TestPainter:
         painter = Painter()
         paint_color = painter.paint_color("red")
 
-        assert paint_color == f"{Fore.PREFIX}{Fore.RED.replace(',', ';')}{painter.SUFFIX}"
+        assert paint_color == f"{Fore.PREFIX}255;0;0{painter.SUFFIX}"
 
     def test_can_paint_existed_multi_words_fore_color(self):
         painter = Painter()
         paint_color = painter.paint_color("light_green")
 
-        assert paint_color == f"{Fore.PREFIX}{Fore.LIGHT_GREEN.replace(',', ';')}{painter.SUFFIX}"
+        assert paint_color == f"{Fore.PREFIX}135;255;95{painter.SUFFIX}"
 
     def test_return_empty_string_when_unexisted_fore_color(self):
         painter = Painter()
@@ -34,19 +34,13 @@ class TestPainter:
         painter = Painter()
         paint_highlight = painter.paint_highlight("white")
 
-        assert (
-            paint_highlight
-            == f"{Highlight.PREFIX}{Highlight.WHITE.replace(',', ';')}{painter.SUFFIX}"
-        )
+        assert paint_highlight == f"{Highlight.PREFIX}255;255;255{painter.SUFFIX}"
 
     def test_can_paint_existed_multi_words_highlight(self):
         painter = Painter()
         paint_highlight = painter.paint_highlight("dark_blue")
 
-        assert (
-            paint_highlight
-            == f"{Highlight.PREFIX}{Highlight.DARK_BLUE.replace(',', ';')}{painter.SUFFIX}"
-        )
+        assert paint_highlight == f"{Highlight.PREFIX}0;0;135{painter.SUFFIX}"
 
     def test_return_empty_string_when_unexisted_highlight(self):
         painter = Painter()
